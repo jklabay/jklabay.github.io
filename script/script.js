5d62813a0e864f32c0379066bb0a7ec04392ce95
jklabay <jeffklein.labay@gmail.com>
2023-03-16 18:24:10 +0800

    Carousel Changes

--- script/script.js
+++ script/script.js
@@ -1,7 +1,26 @@
-$(window).scroll(function() {
-    if ($(document).scrollTop() > 50) {
-      $('.navbar-scroll').addClass('navbar-scrolled');
-    } else {
-      $('.navbar-scroll').removeClass('navbar-scrolled');
+// add class navbarDark on navbar scroll
+const header = document.querySelector('.navbar');
+
+window.onscroll = function() {
+    var top = window.scrollY;
+    if(document.body.scrollTop>=100) {
+        document.getElementById("navbar").style.padding = "30px 10px";
+        document.getElementById("navbar").style.opacity = "0.5";
+        document.getElementById("logo").style.fontSize = "25px";
     }
-  });
+    else {
+        document.getElementById("navbar").style.padding = "80px 10px";
+        document.getElementById("logo").style.fontSize = "35px";
+        document.getElementById("navbar").style.opacity = "1";
+    }
+}
+// collapse navbar after click on small devices
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navbarSupportedContent')
+
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => { new bootstrap.Collapse(menuToggle).toggle() })
+})
+
+
+
